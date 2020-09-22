ad43687299edeb8255000cdb8948ef04be241ded
Chukmarov <61390466+Chukmarov@users.noreply.github.com>
2020-09-22 16:56:24 +0500

    Подключил mangoose. Cоздал схемы user и article

--- app.js
+++ app.js
@@ -1,15 +1,54 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
+const {
+  celebrate, Joi, errors, Segments,
+} = require('celebrate');
+const { login } = require('./controllers/login');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
+mongoose.connect('mongodb://localhost:27017/news-explorer-api', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(helmet());
+
+// app.post('/signin', celebrate({
+//   [Segments.BODY]: Joi.object({
+//     email: Joi.string().required().email(),
+//     password: Joi.string().required().min(8),
+//     name:Joi.string().required().min(2).max(30),
+//   }),
+// }), login);
+
+// app.all('/*', () => {
+//   throw new NotFoundError('Запрашиваемый  ресурс  не  найден');
+// });
+
+// app.use(errors());
+
+
+app.use((err, req, res, next) => {
+  if (err.name === 'ValidationError') {
+    res.status(400).send({ message: 'Проверьте пожалуйста правильность введеных данных' });
+  } else {
+    const { statusCode = 500, message } = err;
+    res.status(statusCode).send({
+      message: statusCode === 500
+        ? 'На сервере произошла ошибка'
+        : message,
+    });
+  }
+});
 
 app.listen(PORT, () => {});
---
+++ models/article.js
@@ -0,0 +1,78 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const articleSchema = new mongoose.Schema({
+
+  keyword: {
+    type: String,
+    minlength: 2,
+    required: true,
+  },
+
+  title: {
+    type: String,
+    minlength: 2,
+    required: true,
+  },
+
+  text: {
+    type: String,
+    minlength: 2,
+    required: true,
+  },
+
+  data: {
+    type: Date,
+    required: true,
+  },
+
+  source: {
+    type: String,
+    required: true,
+  },
+
+  link: {
+    type: String,
+    validate: {
+      validator(link) {
+        return validator.isURL(link);
+      },
+    },
+    required: true,
+  },
+
+  image: {
+    type: String,
+    validate: {
+      validator(link) {
+        return validator.isURL(link);
+      },
+    },
+    required: true,
+  },
+
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    select: false,
+  },
+
+});
+
+// articleSchema.statics.findUserByCredentials = function (email, password) {
+//   return this.findOne({ email }).select('+password')
+//     .then((user) => {
+//       if (!user) {
+//         return Promise.reject(new Error('Неправильные почта или пароль'));
+//       }
+//       return bcrypt.compare(password, user.password)
+//         .then((matched) => {
+//           if (!matched) {
+//             return Promise.reject(new Error('Неправильные почта или пароль'));
+//           }
+//           return user;
+//         });
+//     });
+// };
+
+module.exports = mongoose.model('article', articleSchema);
